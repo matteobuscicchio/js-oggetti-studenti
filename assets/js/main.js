07dd6d3566df63fc370aa0b127caf2322982cc7c
matteo <matteoezio@me.com>
2020-12-03 18:32:37 +0100

    seconda parte esercizio completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,43 +9,34 @@
 
 
 // Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
-// var studente = {
-//     'nome': 'Marco',
-//     'cognome': 'Rossi',
-//     'età' : 25    
-// };
-// for (var key in studente) {
-//     console.log(studente[key]);
-// }
-
-// Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
-var studenteGenericoUno = {
+var studente = {
     'nome': 'Marco',
     'cognome': 'Rossi',
     'età' : 25    
 };
-var studenteGenericoDue = {
-    'nome': 'Maria',
-    'cognome': 'Bianchi',
-    'età' : 30    
-};
-var studenteGenericoTre = {
-    'nome': 'Gerardo',
-    'cognome': 'Albano',
-    'età' : 27    
-};
-var studentArray = [studenteGenericoUno,studenteGenericoDue,studenteGenericoTre];
-console.log(studentArray);
+for (var key in studente) {
+    console.log(studente[key]);
+}
 
-// var index=0;
-// while (index < studentArray.length) {
-//     for (var key in studentArray) {
-//         console.log(studentArray[key]);
-//     }
+// // Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+var studentArray = [
+    {
+        'nome': 'Riccardo',
+        'cognome': 'Fragola',
+        'età' : 25    
+    },
+    {
+        'nome': 'Maria',
+        'cognome': 'Bianchi',
+        'età' : 30    
+    },
+    {
+        'nome': 'Gerardo',
+        'cognome': 'Albano',
+        'età' : 27    
+    }
+];
 
-//     index++;
-// }
-
-for (var key in studentArray) {
-    console.log(studentArray[key]);
+for (const key in studentArray) {
+    console.log(studentArray[key].nome, studentArray[key].cognome);
 }
